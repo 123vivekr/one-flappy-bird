12fafe67c915268c6b06e7d08b6a56b4a1b93357
123vivekr <123vivekr@gmail.com>
2018-04-26 15:07:27 +0530

    init background

--- flappybird.py
+++ flappybird.py
@@ -1,11 +1,22 @@
 import pygame
+from os import path
 
-SCREEN_HEIGHT, SCREEN_WIDTH = 512, 284*2
+SCREEN_HEIGHT, SCREEN_WIDTH = 512, 284*2 #bg image dim: 284x512
 
 def main():
+    #init window
     pygame.init()
+    screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
     pygame.display.set_caption("One Flappy Bird")
-    pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+
+    # init background
+    bgImg = pygame.image.load(path.join('.','assets','background.png'))
+    screen.blit(bgImg, (0,0))
+    screen.blit(bgImg, (284, 0))
+
+
+    pygame.display.update();
+    pygame.time.delay(10)
 
 if __name__ == '__main__':
     main()
