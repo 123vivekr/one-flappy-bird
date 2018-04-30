a36ec27b4c398a6f9580fe777a3132c96b35638b
123vivekr <123vivekr@gmail.com>
2018-04-30 10:19:52 +0530

    bird motion and game loop

--- flappybird.py
+++ flappybird.py
@@ -14,15 +14,28 @@ class Bird(pygame.sprite.Sprite):
     def display(self, screen):
         screen.blit(self.imgdown, (self.x, self.y))
 
+    def fall(self):
+        self.y += 15
+
+    def jump(self):
+        self.y-=10
+
 class Pipe(pygame.sprite.Sprite):
 
     def init(self, img):
         super(Bird, self).__init__()
         self.imgbody, self.imgend = img
         self.imgbody_mask, self.imgdown_mask = pygame.mask.from_surface(self.imgbody), pygame.mask.from_surface(self.imgend)
+    
+    
 
 def load_image():
     return {f[:-4]: pygame.image.load(os.path.join('.','assets', f)).convert_alpha() for f in os.listdir(os.path.join('.', 'assets'))}
+
+def background(screen, bg, ground):
+    screen.blit(bg, (0,0))
+    screen.blit(bg, (284, 0))
+    screen.blit(ground, (0, SCREEN_HEIGHT-60))
 
 def main():
     #init window
@@ -32,20 +45,27 @@ def main():
 
     # cache all images
     image = load_image()
+    image['ground'] = pygame.transform.scale(image['ground'], (int(SCREEN_WIDTH), int(SCREEN_HEIGHT/5))) #scale ground image
 
     # init background
-    screen.blit(image['background'], (0,0))
-    screen.blit(image['background'], (284, 0))
-    ground_scale = pygame.transform.scale(image['ground'], (int(SCREEN_WIDTH), int(SCREEN_HEIGHT/5)))
-    screen.blit(ground_scale, (0, SCREEN_HEIGHT-90))
 
     #initialise Bird
-    birdy = Bird(0, SCREEN_HEIGHT/2, (image['bird_wing_up'], image['bird_wing_down']))
+    birdy = Bird(SCREEN_WIDTH/2-50, (SCREEN_HEIGHT/2)-20, (image['bird_wing_up'], image['bird_wing_down']))
 
-    birdy.display(screen)
+    crashed = False
+    clock = pygame.time.Clock()
 
-    pygame.display.update();
-    pygame.time.delay(1000)
+    #game loop
+    while not crashed:
+        background(screen, image['background'], image['ground'])
+        birdy.display(screen)
+        pygame.display.update();
+        birdy.fall()
+        
+        print("loop")
+        clock.tick(30)
+
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
